742a04d80dfa1e092d27b4c2979def2a49be993b
Kenneth Lee <kennethslee.613@gmail.com>
2020-01-20 11:42:57 -0500

    added image on earth and rotating sun

--- flat-earth.js
+++ flat-earth.js
@@ -1,35 +1,78 @@
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, earth, sun, sunlight;
  
 init();
 animate();
  
 function init() {
- 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
- 
     scene = new THREE.Scene();
- 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
- 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
- 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
- 
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    geometry = new THREE.CylinderGeometry(300, 300, 1, 100);
+    var texture = new THREE.TextureLoader().load('img/earth.jpg');
+    texture.crossOrigin = true;
+    material = new THREE.MeshBasicMaterial({
+        map: texture
+    });
+    material.needsUpdate = true;
+    material.map.minFilter = THREE.LinearFilter;
+
+    earth = new THREE.Mesh(geometry, material);
+    scene.add(earth);
+
+    geometry = new THREE.SphereGeometry(10, 32, 32);
+    material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    material.needsUpdate = true;
+    sun = new THREE.Mesh(geometry, material);
+    sun.position.y = 75;
+    sun.position.x = 125;
+    sun.position.z = 125;
+    scene.add(sun);
+
+
+    camera.position.x = 0;
+    camera.position.y = 500;
+    camera.position.z = 0;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    // camera.position.y = 100;
+    // camera.position.z = 500;
 }
  
 function animate() {
+    requestAnimationFrame(animate);
  
-    requestAnimationFrame( animate );
- 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
- 
-    renderer.render( scene, camera );
- 
+    // earth.rotation.x += 0.01;
+    // earth.rotation.y += 0.02;
+    const vector = new THREE.Vector3(0, 75, 0);
+    rotateAboutPoint(sun, vector, vector.normalize(), 0.02, false);
+    // rotateAboutPoint(sunlight, vector, vector.normalize(), 0.02, false);
+
+    renderer.render(scene, camera);
+}
+
+// obj - your object (THREE.Object3D or derived)
+// point - the point of rotation (THREE.Vector3)
+// axis - the axis of rotation (normalized THREE.Vector3)
+// theta - radian value of rotation
+// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
+function rotateAboutPoint(obj, point, axis, theta, pointIsWorld) {
+    pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
+
+    if(pointIsWorld){
+        obj.parent.localToWorld(obj.position); // compensate for world coordinate
+    }
+
+    obj.position.sub(point); // remove the offset
+    obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+    obj.position.add(point); // re-add the offset
+
+    if(pointIsWorld){
+        obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+    }
+
+    obj.rotateOnAxis(axis, theta); // rotate the OBJECT
 }
